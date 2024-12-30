16e82fc85933106b7122c323106d4fecc7145744
baranwang <me@baran.wang>
2024-12-31 00:25:57 +0800

    feat: add zod validation and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { URL } from "node:url";
+import { z } from "zod";
 
 const dlerFetch = <T,>(urlPath: string, body: any = {}): Promise<T> => fetch(`https://dler.cloud${urlPath}`, {
 	method: 'POST',
@@ -8,6 +9,12 @@ const dlerFetch = <T,>(urlPath: string, body: any = {}): Promise<T> => fetch(`ht
 	},
 }).then((res) => res.json())
 
+const paramsSchema = z.object({
+	email: z.string({ message: 'email is required' }),
+	passwd: z.string({ message: 'password is required' }),
+	type: z.enum(['smart', 'ss', 'vmess', 'trojan']).optional().default('smart'),
+})
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const url = new URL(request.url)
@@ -15,13 +22,15 @@ export default {
 		const email = request.headers.get('X-Dler-Email') || url.searchParams.get('email')
 		const passwd = request.headers.get('X-Dler-Password') || url.searchParams.get('passwd')
 
-		if (!email || !passwd) {
-			return new Response('email and password are required', { status: 400 })
+		const params = paramsSchema.safeParse({ email, passwd, type: url.searchParams.get('type') ?? undefined })
+
+		if (!params.success) {
+			return new Response(params.error.errors[0].message, { status: 400 })
 		}
 
 		const loginResp = await dlerFetch<{ ret: number; data: { token: string }, msg: string }>('/api/v1/login', {
-			email,
-			passwd,
+			email: params.data.email,
+			passwd: params.data.passwd,
 			token_expire: 1
 		})
 
@@ -29,9 +38,9 @@ export default {
 			return new Response(loginResp.msg, { status: loginResp.ret })
 		}
 
-		const managedResp = await dlerFetch<{ smart: string }>('/api/v1/managed/clash', { access_token: loginResp.data.token })
+		const managedResp = await dlerFetch<{ [key: string]: string }>('/api/v1/managed/clash', { access_token: loginResp.data.token })
 
-		const resp = await fetch(managedResp.smart)
+		const resp = await fetch(managedResp[params.data.type])
 
 		await dlerFetch('/api/v1/logout', { access_token: loginResp.data.token })
 
